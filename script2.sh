510735bd19d4943c5ab644ed41a1be3d479ac0dd
KajsaGeorgii <georgii.hellberg@gmail.com>
2020-11-15 15:17:26 +0100

    ändrar fil som ej är färdig

--- script2.sh
+++ script2.sh
@@ -1,1 +1,5 @@
-function single_nucleotides { echo "Input file:"; read file; local var=$(cat $file | awk '{print $4}' | grep '^.$' | awk  '{ seen[$0] += 1 } END { for (i in seen) print seen[i],i }' | sort -k2,2 -n); local var2=$(echo "$var" | cut -d" " -f 1); local var3=$(cat $file | wc -l); local var4=$(while IFS= read -r line; do echo $((line / var3)); done <<< "$var2"); local var5=$(echo "$var" | cut -d" " -f 2); awk -v a="$var4" -v b="$var5" 'BEGIN {print a,b}';}
+function single_nucleotides { 
+	echo "Input file:"; read file; 
+	local var=$(echo $file | awk '{print $4}' | grep '^.$' | awk  '{ seen[$0	] += 1 } END { for (i in seen) print seen[i],i }' | sort -k2,2 -n | awk 	-v a="$(echo $file | wc -l)" '{print $0/a; print $2}'); 
+	awk -v a="$var" 'BEGIN {print a}';
+	}
